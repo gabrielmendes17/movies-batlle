1c23afa47b3f6364b7502a3b25552cb8467299dd
gabrielmendes17 <gabrielmendes17@gmail.com>
2022-03-21 12:56:09 -0300

    fix: app name

--- src/main/java/br/com/lets_code/Movies/Batlle/MoviesBatlleApplication.java
+++ src/main/java/br/com/lets_code/Movies/Batlle/MoviesBatlleApplication.java
@@ -1,13 +1,13 @@
-package br.com.lets_code.Movies.Battlle;
+package br.com.lets_code.Movies.Batlle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoviesBattlleApplication {
+public class MoviesBatlleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesBattlleApplication.class, args);
+		SpringApplication.run(MoviesBatlleApplication.class, args);
 	}
 
 }
--- src/test/java/br/com/lets_code/Movies/Batlle/MoviesBattlleApplicationTests.java
+++ src/test/java/br/com/lets_code/Movies/Batlle/MoviesBattlleApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.lets_code.Movies.Battlle;
+package br.com.lets_code.Movies.Batlle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MoviesBattlleApplicationTests {
+class MoviesBatlleApplicationTests {
 
 	@Test
 	void contextLoads() {
