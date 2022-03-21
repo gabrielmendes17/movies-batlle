8d39063241bebae67049c081997bdb8b6a78c8f6
gabrielmendes17 <gabrielmendes17@gmail.com>
2022-03-21 12:44:32 -0300

    feat: created project with spring initializr

---
+++ src/main/java/br/com/lets_code/Movies/Battlle/MoviesBattlleApplication.java
@@ -0,0 +1,13 @@
+package br.com.lets_code.Movies.Battlle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesBattlleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesBattlleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lets_code/Movies/Battlle/MoviesBattlleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lets_code.Movies.Battlle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBattlleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
