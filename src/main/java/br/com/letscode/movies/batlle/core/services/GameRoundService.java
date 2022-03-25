32b1a9df0721771d6f7b33dfeca2f167a7a24b23
gabrielmendes17 <gabrielmendes17@gmail.com>
2022-03-25 17:23:54 -0300

    fix: film rating comparing bug

--- src/main/java/br/com/letscode/movies/batlle/core/services/GameRoundService.java
+++ src/main/java/br/com/letscode/movies/batlle/core/services/GameRoundService.java
@@ -4,8 +4,12 @@ import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +28,8 @@ import br.com.letscode.movies.batlle.presenter.rest.dtos.request.QuizzGuessReque
 @Service
 public class GameRoundService {
 
+    private static final Logger log = LoggerFactory.getLogger(GameRoundService.class);
+
     @Autowired
     GameRoundRepository gameRoundRepository;
 
@@ -39,8 +45,12 @@ public class GameRoundService {
     @Autowired
     UserService userService;
     
-    private boolean guessSuccess(List<Film> films) {
-        return films.get(0).getRating() > films.get(1).getRating();
+    private boolean guessSuccess(List<Film> films,QuizzGuessRequest quizzRequest) {
+        Film winningMovie = films.stream().filter(film -> {
+            return film.getId().equals(quizzRequest.getGuessWinningMovieId());
+        }).collect(Collectors.toList()).get(0);
+        Film losingMovie = films.stream().filter(film -> film.getId().equals(quizzRequest.getGuessLosingMovieId())).collect(Collectors.toList()).get(0);
+        return winningMovie.getRating() > losingMovie.getRating();
     }
 
     public Boolean handlePlayerGuess(QuizzGuessRequest quizzRequest, Principal principal) throws GameMatchOpenNotFound, WrongFilmCombinationGuess {
@@ -49,12 +59,14 @@ public class GameRoundService {
         FilmCombination filmCombination = filmCombinationRepository.findFirst1ByUserIdAndAttemptsLessThanOrderByIdAsc(user.getId(), 3).get();
         validatePlayerGuess(quizzRequest, filmCombination);
         List<Film> films = filmsRepository.findAllById(Arrays.asList(quizzRequest.getGuessWinningMovieId(), quizzRequest.getGuessLosingMovieId()));
-        if (this.guessSuccess(films)) {
+        if (this.guessSuccess(films, quizzRequest)) {
+            log.info("Player guess success {}, {}, {}", films.get(0).getRating(), films.get(1).getRating(), this.guessSuccess(films, quizzRequest));
             gameRoundRepository.save(new GameRound(gameMatch, 1));
             filmCombination.setAttempts(3);
             filmCombinationRepository.save(filmCombination);
             return true;
         }
+        log.info("Player guess failure {}, {}, {}", films.get(0).getRating(), films.get(1).getRating(), this.guessSuccess(films, quizzRequest));
         gameRoundRepository.save(new GameRound(gameMatch, 0));
         filmCombination.setAttempts(filmCombination.getAttempts()+1);
         filmCombinationRepository.save(filmCombination);
@@ -65,8 +77,10 @@ public class GameRoundService {
         Supplier<Stream<String>> filmsStreamSupplier = () ->  Arrays.asList(String.valueOf(filmCombination.getFirstFilmCombination()), String.valueOf(filmCombination.getSecondFilmCombination())).stream();
         boolean isPlayerGuessingTheRightFilmCombination = filmsStreamSupplier.get().anyMatch(quizzRequest.getGuessLosingMovieId()::equals) && filmsStreamSupplier.get().anyMatch(quizzRequest.getGuessLosingMovieId()::equals);
         if (isPlayerGuessingTheRightFilmCombination) {
+            log.info("Player is trying to guess with wrong films ids {}", filmsStreamSupplier.get());
             return;
         }
+        log.info("Films id validation passed {} ", filmsStreamSupplier.get());
         throw new WrongFilmCombinationGuess();
     }
 }
